9cfd0bb43ed183139b91c20226e5f7c88676e197
Etrav05 <Etravis090705@gmail.com>
2025-11-03 20:28:19 -0500

    Created new folder: Data. This folder contains our AppDbContext Class
    This class  is going to be our database session/gateway. Whenever the app wants to query, insert, update, or delete data, it does it through this class
    
    It contains a constructor with has 1 parameter: DbContextOptions<AppDbContext> options
    
    This parameter is basically a config package that tells our EF Core how to connect to the database. The database connection settings (options) are usually configured in a Program.cs, so I will do that soon

---
+++ MyProjectTemplate.API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+// This is going to be our database session/gateway
+// Whenever this app wants to query, insert, update, or delete data, it does it through this class
+namespace MyProjectTemplate.API.Data
+{
+    public class AppDbContext : DbContext // Inherits from DbContext, this is the main EF Core class responsible for both managing database connections and tracking data changes
+    {
+        // Basically a config package that tells our EF Core how to connect to the database
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        // Example table syntax we can use
+        // public DbSet<RandomSensorReading> SensorReadings { get; set; }
+    }
+}

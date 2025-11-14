b4cdcec36c555f432c3933455ede07b0937d75ce
Etrav05 <Etravis9851@conestogac.on.ca>
2025-11-13 21:02:24 -0500

    Started creating a controller for the submarin
    I am working on this conrtollers functionality to send data to our database

---
+++ MyProjectTemplate.API/Controllers/SubmarineController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyProjectTemplate.API.Models;
+using MyProjectTemplate.API.Data;
+
+namespace MyProjectTemplate.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] // Route: /SubData
+    public class SubDataController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public SubDataController(AppDbContext db) // Setting up the database connection
+        {
+            _db = db;
+        }
+
+        // POST /SubData
+        [HttpPost]
+        public IActionResult CreateSubData([FromBody] SubDatum data) // This will accept a .json representing a SubDatum
+        {
+            _db.SubData.Add(data); // Saves that .json data into our database through EF Core
+            _db.SaveChanges(); 
+
+            return Ok(data);
+        }
+    }
+}

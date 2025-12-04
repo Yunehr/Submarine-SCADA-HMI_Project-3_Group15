5de7ee523b841e49d447f7db8c86a6a5d45f0c9d
sebastian <sebastianhsolorzano@gmail.com>
2025-12-04 13:16:52 -0500

    fixed controls tests, started movement tests, added limits to navi

--- MyProjectTemplate.API/SubMovement/SubNav/Navi.cs
+++ MyProjectTemplate.API/SubMovement/SubNav/Navi.cs
@@ -9,6 +9,12 @@ namespace MyProjectTemplate.API.SubSubController.SubNav
         public double Y { get; private set; }
         public double Z { get; private set; }
 
+        private const double maplimit = 500.0; //decided by group
+        private const double depthlimit = -440.0; //somewhat arbitrary; an Ohio class's test depth is 240m,
+        //and with a US Navy test depth 2/3 of its design depth, it should be fine until 360m.
+        //of course, it can almost certainly go deeper than that (which they obviously wouldn't tell us)
+        //but this seems reasonable enough
+
         //cant believe I forgot the constructor
         public Navi()
         {
@@ -21,10 +27,28 @@ namespace MyProjectTemplate.API.SubSubController.SubNav
         {
             //would like add coefficients for this so we aren't moving at 100<unit> per frame rather than hour/minute/more reasonable measurment
             //its all abstract though so it doesn't really matter i guess
-            X += Vx;
-            Y += Vy;
-            Z += Vz;
 
+
+            X += Vx;
+            //we're adding to, not just replacing, so we can't do it like i did in controlelements
+            if (X > maplimit) 
+                X = maplimit;
+            else if (X < -maplimit)
+                X = -maplimit;
+
+            Y += Vy;
+            if (Y > maplimit)
+                Y = maplimit;
+            else if (Y < -maplimit)
+                Y = -maplimit;
+
+            Z += Vz;
+            if (Z > 0.0)
+                Z = 0;
+            else if (X < depthlimit)
+                X = depthlimit;
+
+            //I don't actually know what consequences should occur if we reach the limits, so i'll just leave it like this for now
         }
 
     }
---
+++ Submarine-SCADA-HMI.Tests/SubControlsTests/MovementTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Submarine_SCADA_HMI.Tests.SubControlsTests
+{
+    internal class MovementTests
+    {
+    }
+}
